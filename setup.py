491392d853cd38a911187e580eea324575ca5ad9
Phani Kumar <phani4passion@gmail.com>
2025-09-28 20:35:50 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_advice_chatbot",
+    version="0.1.0",
+    author="Anil Pavan Phani Kumar",
+    author_email="phani4passion@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
